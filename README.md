afecc3c29e10b8170c2d31f4c99e4ddd4fc236da
vikd <vikd@vikas-MacBook-Pro.local>
2022-06-24 00:27:20 +0530

    User will be able to use refresh functionality as a module

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## What is this?
 
-This is an API to generate a fresh API key for Supercell games for your current IP. It supports [Clash Royale](https://developer.clashroyale.com), [Clash of Clans](https://developer.clashofclans.com) and [Brawl Stars](https://developer.brawlstars.com).
+This is an module to generate a fresh API key for Supercell games for your current IP. It supports [Clash Royale](https://developer.clashroyale.com), [Clash of Clans](https://developer.clashofclans.com) and [Brawl Stars](https://developer.brawlstars.com).
 
 ## Why?
 
@@ -8,15 +8,47 @@ Those API's has a limitation of **one API key per IP** and a maximum of 10 API k
 
 ## How to use it?
 
-You can use this API by making a POST request to the following URL `https://get-sc-key-production.up.railway.app` adding in the body your user credentials and the game id you want to generate a key for. Example:
-```json
-{
-  "game": "clashroyale",
-  "email": "yourmail",
-  "password": "yourpassword"
-}
+You can use this module by simply adding it to your project and pass game name, your email and password.
 ```
-Then you will receive a JSON response like this: 
+const CTR = require('coc-token-refresher')
+async function f(){
+    try{
+        let response = await CTR.getNewAPI("clashroyale","yourmail","yourpassword")
+        console.log(response)
+    }catch(err){
+        console.log(err)
+    }
+};
+f();
+```
+Or If you don't want to show your email and password in the code, you can set environment variables for your email and password, they will load automatically. Example:   
+
+TERMINAL:   
+```
+//On Windows
+set email="your@email.com"
+set password="yourPassword"
+
+//On MAC 
+export email="your@email.com"
+export password="yourPassword"
+```
+JavaScript:   
+```
+const CTR = require('coc-token-refresher')
+async function f(){
+    try{
+        let response = await CTR.getNewAPI("clashroyale")
+        //This is enough. If this doesn't work for you, then can use the below code.
+        //let response = await CTR.getNewAPI("clashroyale",process.env.email,process.env.password)
+        console.log(response)
+    }catch(err){
+        console.log(err)
+    }
+};
+f();
+```
+It will return a JSON object like this: 
 ```json
 {
   "name": "Key generated at 8/2/2022 6:19:26 p. m.",
@@ -28,16 +60,10 @@ Then you will receive a JSON response like this:
 }
 ```
     Supported game IDs: clashroyale, clashofclans, brawlstars, cr, coc, bs
-    Mirror API URLs: https://get-sc-key.vercel.app, https://get-sc-key.herokuapp.com
-
-## Run locally
-- `$ npm run install`
-- `$ npm run start`
-- Then you can make your requests to `https://localhost:1234`
 
 ## How it works?
 
-This API login into the game developer portal with your credentials, check if exists a key for your IP and if not, **delete the first key** and generate a new one for your IP. Otherwise, it will return the existing key. The logic is inspired on [TheLearneer/supercell-api](https://github.com/TheLearneer/supercell-api). The IP is obtained from [ipify](https://api.ipify.org/).
+This module login into the game developer portal with your credentials, check if exists a key for your IP and if not, **delete the first key** and generate a new one for your IP. Otherwise, it will return the existing key. The logic is inspired on [TheLearneer/supercell-api](https://github.com/TheLearneer/supercell-api). The IP is obtained from [ipify](https://api.ipify.org/).
 
 ## Optional parameters
 ### `whitelist`
--- index.js
+++ index.js
@@ -1,89 +1,78 @@
 require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
 const { login, getKeys, revokeKey, createKey, getCookie, getIP } = require('./utils')
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
+async function getNewAPI(game, email, password, whitelist = [], fixedIp = ''){
+    try {
+        if (!email || !password) {
+            if(!process.env.email || !process.env.password) {
+                throw new Error('email and password are required')
+            }else {
+                email = process.env.email
+                password = process.env.password
+            }
+            
+        }
 
-app.post('/', async (req, res) => {
-  try {
-    let { game } = req.body
-    const { email, password, whitelist = [], fixedIp = '' } = req.body
+        if (!game) {
+            throw new Error('game id is required')
+        }
 
-    if (!email || !password) {
-      return res.status(400).json({ error: 'email and password are required' })
+        if (game === 'cr') game = 'clashroyale'
+        if (game === 'coc') game = 'clashofclans'
+        if (game === 'bs') game = 'brawlstars'
+
+        if (game !== 'clashroyale' && game !== 'clashofclans' && game !== 'brawlstars') {
+            throw new Error('game id must be either "clashroyale", "clashofclans", "brawlstars", "cr", "coc" or "bs"')
+        }
+
+        const baseUrl = `https://developer.${game}.com/api`
+
+        // login
+        const loginResponse = await login({ baseUrl, email, password })
+        if (loginResponse?.error) {
+            throw new Error(loginResponse)
+        }
+
+        // get cookie
+        const cookie = await getCookie(loginResponse)
+
+        // get current keys:
+        const savedKeys = await getKeys({ baseUrl, cookie })
+
+        // get current IP
+        const ip = fixedIp || await getIP()
+
+        // check if exists a key with the same IP:
+        const keyWithSameIP = savedKeys.find(key => key.cidrRanges.includes(ip))
+
+        let validApiKey
+        if (keyWithSameIP) {
+            validApiKey = keyWithSameIP
+        } else {
+            // revoke the first key which is not whitelisted
+            if (savedKeys.length === 10) {
+                const keyToRevoke = savedKeys.find(key => !whitelist.includes(key.name))
+                await revokeKey({ baseUrl, cookie, keyToRevoke })
+            }
+
+            // create new key
+            const newKey = await createKey({ baseUrl, cookie, ips: [ip] })
+            if (newKey?.error) {
+                throw new Error(newKey)
+            }
+
+            validApiKey = newKey.key
+        }
+
+        return {
+            name: validApiKey.name,
+            description: validApiKey.description,
+            ipRange: validApiKey.cidrRanges,
+            key: validApiKey.key
+        }
+    } catch (error) {
+        throw error 
     }
+}
 
-    if (!game) {
-      return res.status(400).json({ error: 'game id is required' })
-    }
-
-    if (game === 'cr') game = 'clashroyale'
-    if (game === 'coc') game = 'clashofclans'
-    if (game === 'bs') game = 'brawlstars'
-
-    if (game !== 'clashroyale' && game !== 'clashofclans' && game !== 'brawlstars') {
-      return res.status(400).json({ error: 'game id must be either "clashroyale", "clashofclans", "brawlstars", "cr", "coc" or "bs"' })
-    }
-
-    const baseUrl = `https://developer.${game}.com/api`
-
-    // login
-    const loginResponse = await login({ baseUrl, email, password })
-    if (loginResponse?.error) {
-      return res.status(401).send(loginResponse)
-    }
-
-    // get cookie
-    const cookie = await getCookie(loginResponse)
-
-    // get current keys:
-    const savedKeys = await getKeys({ baseUrl, cookie })
-
-    // get current IP
-    const ip = fixedIp || await getIP()
-
-    // check if exists a key with the same IP:
-    const keyWithSameIP = savedKeys.find(key => key.cidrRanges.includes(ip))
-
-    let validApiKey
-    if (keyWithSameIP) {
-      validApiKey = keyWithSameIP
-    } else {
-      // revoke the first key which is not whitelisted
-      if (savedKeys.length === 10) {
-        const keyToRevoke = savedKeys.find(key => !whitelist.includes(key.name))
-        await revokeKey({ baseUrl, cookie, keyToRevoke })
-      }
-
-      // create new key
-      const newKey = await createKey({ baseUrl, cookie, ips: [ip] })
-      if (newKey?.error) {
-        return res.status(500).send(newKey)
-      }
-
-      validApiKey = newKey.key
-    }
-
-    res.json({
-      name: validApiKey.name,
-      description: validApiKey.description,
-      ipRange: validApiKey.cidrRanges,
-      key: validApiKey.key
-    })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error })
-  }
-})
-
-const PORT = process.env.PORT || 1234
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-module.exports = app
+module.exports={getNewAPI}
